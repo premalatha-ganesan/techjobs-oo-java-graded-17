cd076087cf89fd95d2675e65007499f8c71c1dbd
Latharagav <tgpremlatha@gmail.com>
2024-10-16 13:07:50 -0500

    GA # 2 - Task 6

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,4 +1,44 @@
 package org.launchcode.techjobs.oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id= nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField jobField)) return false;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
